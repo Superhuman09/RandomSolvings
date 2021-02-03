2c455d0c07b1d6e1b4d8a6eba0d3aef31cb58404
timur <email@email>
2021-02-03 13:09:58 +0500

    first save

---
+++ script.js
@@ -0,0 +1,38 @@
+function myRandom(min, max) {
+    let random = Math.random() * (max + 1 - min) + min
+    let floor = Math.floor(random)
+
+    return floor
+}
+
+let minInt = +prompt('Введите минимальное число')
+let maxInt = +prompt('Введите максимальное число')
+
+let answerUser = 0
+let answerUserMinus = 0
+for(let i = 0; i < 10; i++) {
+    let num1 = myRandom(minInt, maxInt)
+    let num2 = myRandom(maxInt, maxInt)
+    let symbol = myRandom(1, 4);
+    let symbolStr = ''
+    let answerPr
+    if (symbol === 1) {
+        symbolStr = ' + ';
+        answerPr = num1 + num2
+    } else if(symbol === 2) {
+        symbolStr = ' - ';
+        answerPr = num1 - num2
+    } else if(symbol === 3) {
+        symbolStr = ' * ';
+        answerPr = num1 * num2
+    }else {
+        symbolStr = ' / ';
+        answerPr = num1 / num2
+    }
+    let pr = +prompt(num1 + symbolStr + num2 + ' =')
+    let answer = answerPr === pr ? 'Good boy' : 'Mistake'
+    num1 + num2 === pr ? answerUser++ : answerUserMinus++
+    alert(num1 + symbolStr + num2 + ' = ' + answerPr + ' Your answer ' + pr + '. ' + answer)
+}
+
+alert('Right answers: ' + answerUser + '! Wrong answers: ' + answerUserMinus)
